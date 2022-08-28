04d67f3bba86324f1b1479673e10793ba8a6d0ac
raonixx <108378080+raonixx@users.noreply.github.com>
2022-08-28 12:32:50 -0300

    Update calculadoraWhile.py

--- calculadoraWhile.py
+++ calculadoraWhile.py
@@ -1,6 +1,10 @@
-#
-#
-#
+#ATIVIDADE 11
+#Faça uma função calculadora que os números e as operações serão feitas pelo usuário. O código deve ficar rodando infinitamente até que o usuário escolha a opção de sair. No início, o programa mostrará a seguinte lista de operações:
+1: Soma
+2: Subtração
+3: Multiplicação
+4: Divisão
+0: Sair
 
 
 print('calculadora')
