c7bf2a7c7e956f3ec8fcb14da8bbffe39e64e4d6
raonixx <108378080+raonixx@users.noreply.github.com>
2022-09-13 23:17:46 -0300

    Create manipulacao_de _string.py

---
+++ manipulacao_de _string.py
@@ -0,0 +1,14 @@
+# usando métodos de manipulação string em python
+
+#Código 1
+str =  "raoni xavier"
+print(str)
+print(str.upper())
+
+#Código 2
+str = "RAONI XAVIER"
+print(str.lower())
+
+#Código 3
+str = "RAONI"
+print(str[0])
